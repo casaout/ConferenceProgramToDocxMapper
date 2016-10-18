cc39f2232d6ecf44b20ea1326f74236a5c36be71
Andre Meyer <info@andre-meyer.ch>
2016-10-18 14:19:03 +0200

    + added initial project + added gitignore

---
+++ ConferenceProgramToDocxMapper/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Office.Interop.Word;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace ConferenceProgramToDocxMapper
+{
+    public class Program : IDisposable
+    {
+        private Application _wordApplication;
+        private Document _word;
+        private bool _templateUsed;
+        private string _fileSavePath;
+
+        public Program(string fileSavePath, string templatePath = null)
+        {
+            _fileSavePath = fileSavePath;
+            _wordApplication = new Application();
+
+            // open existing word file (to re-use styles)
+            if (templatePath != null && File.Exists(templatePath))
+            {
+                _word = _wordApplication.Documents.Open(templatePath);
+                _templateUsed = true;
+            }
+            // create new word file
+            else
+            {
+                _word = _wordApplication.Documents.Add();
+            }
+
+            _wordApplication.Visible = true; // show created word document when program runs
+        }
+
+        public void SaveAndCloseProgram()
+        {
+            _wordApplication.ActiveDocument.SaveAs(_fileSavePath, WdSaveFormat.wdFormatDocument);
+            // _wordApplication.ActiveDocument.Save();
+            _word.Close();
+        }
+
+        public void Dispose()
+        {
+            if (_wordApplication != null)
+            {
+                _wordApplication.Quit();
+                Marshal.FinalReleaseComObject(_wordApplication);
+            }
+            //GC.SuppressFinalize(this);
+        }
+
+        public void AddDaySeparator(string text)
+        {
+            AddParagraph("— " + text + " —", "S Date");
+        }
+
+        public void AddSessionTitle(string title, string time, string location, string chair = null)
+        {
+            if (string.IsNullOrEmpty(location)) location = "location unknown";
+            var timeLocString = time + ", " + location;
+
+            AddParagraph(title, "S Title");
+            AddParagraph(timeLocString, "S Title");
+            if (chair != null) AddParagraph(chair, "S Session Chair");
+        }
+
+        public void AddBreak(string title, string time)
+        {
+            AddParagraph(title, "S Break");
+            AddParagraph(time, "S Break");
+        }
+
+        public void AddPaper(string title, string authors)
+        {
+            AddParagraph(title, "P Title");
+            AddParagraph(authors, "P Author");
+        }
+
+        private void AddParagraph(string text, object styleName = null)
+        {
+            var paragraph = _word.Paragraphs.Add(); // paragraph.Content.Paragraphs.Add();
+            paragraph.Range.Text = text;
+
+            if (styleName != null)
+            {
+                paragraph.set_Style(ref styleName); //_document.Styles[styleName]);
+            }
+
+            paragraph.Range.InsertParagraphAfter();
+        }
+
+    }
+}
---
+++ ConferenceProgramToDocxMapper/RunMapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Office.Interop.Word;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace ConferenceProgramToDocxMapper
+{
+    class RunMapper
+    {
+        //private const string _path = @"C:\Users\André\Desktop\Yearbook\Program\Testing";
+        //private const string _fileName = "program-new"; // .docx
+        private const string _templatePath = @"C:\Users\André\Desktop\Yearbook\Program\Testing\program.docx";
+        private const string _exportFilePath = @"C:\Users\André\Desktop\Yearbook\Program\Testing\program-new"; // no file extension! .docx";
+
+
+        static void Main(string[] args)
+        {
+            var program = new Program(_exportFilePath, _templatePath); // initialize interop word program
+
+            program.AddDaySeparator("Saturday, May 14");
+            program.AddSessionTitle("Round Table on Privacy Policies/Protocols", "Sat, May 14, 1o:10 - 10:30", "Ballroom B", "Moderator: Tom Zimmermann");
+            program.AddBreak("Morning Break", "Sat, May 14, 10:30 - 11:00");
+            program.AddPaper("Raising MSR Researchers: An Experience Report on Teaching a Graduate Seminar Course in Mining Software Repositories (MSR)", "Ahmed E. Hassan (Queen's University, Canada)");
+            program.AddPaper("Interactive Exploration of Developer Interaction Traces using a Hidden Markov Model", "Kostadin Damevski, Hui Chen, David Shepherd, and Lori Pollock (Virginia Commonwealth University, USA; Virginia State University, USA; ABB, Inc, USA; University of Delaware, USA)");
+
+            program.SaveAndCloseProgram();
+        }
+
+        
+    }
+}
